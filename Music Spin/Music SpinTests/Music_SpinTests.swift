c962f6f8f3eb53195cb9e0979f43cc8e94243628
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 21:57:40 -0400

    Add Music Spin/Music SpinTests/Music_SpinTests.swift

---
+++ Music Spin/Music SpinTests/Music_SpinTests.swift
@@ -0,0 +1,17 @@
+//
+//  Music_SpinTests.swift
+//  Music SpinTests
+//
+//  Created by Elliot Williams on 2025-07-13.
+//
+
+import Testing
+@testable import Music_Spin
+
+struct Music_SpinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
